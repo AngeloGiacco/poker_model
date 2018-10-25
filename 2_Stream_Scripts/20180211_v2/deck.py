7aa87b0a9b6ac3ed08c1ba8faf7aa91345cc7b2d
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-10-25 14:53:15 +0200

    Update deck.py

--- 2_Stream_Scripts/20180211_v2/deck.py
+++ 2_Stream_Scripts/20180211_v2/deck.py
@@ -18,7 +18,6 @@ class Deck(object):
 
             locals()['card%s' % i] = card.Card(figure + color)
             i += 1
-
     def __init__(self):
         ''' deck initialization '''
 
@@ -31,38 +30,39 @@ class Deck(object):
 
     def show_deck(self):
         ''' show deck '''
-        
-        return self.__deck
+
+        return(" ".join([c.show_card() for c in self.__deck]))
 
     def nth_card(self, n):
         ''' show the n-th card from deck '''
-        
+
         try:
             return self.__deck[n]
-        
         except BaseException:
             print('Wrong index')
-            pass
-    
+            return card.Card("0X")
+
     def draw_by_number(self, nr):
         ''' pick card from deck by nr '''
-        
+
         try:
             pick = self.__deck[nr]
+            if pick == "OX":
+                raise BaseException
             self.__deck.remove(pick)
             return pick
 
         except BaseException:
             print('Wrong index')
-            pass
+            return card.Card("0X")
 
     def draw_by_name(self, name):
         ''' pick card from deck by name '''
 
         flag = 0
         for pick in self.__deck:
-        
-            if pick.show() == name:
+
+            if pick.show_card() == name:
                 self.__deck.remove(pick)
                 flag = 1
                 break
@@ -76,10 +76,10 @@ class Deck(object):
 
     def make_board(self):
         ''' make a random board '''
-        
+
         board = []
         for i in range(5):
-        
+
             random_number = random.randint(0, len(self.__deck) - 1)
             board.append(self.draw_by_number(random_number))
 
@@ -87,11 +87,15 @@ class Deck(object):
 
     def make_hand(self):
         ''' make a random hand '''
-        
+
         hand = []
         for i in range(2):
-        
+
             random_number = random.randint(0, len(self.__deck) - 1)
             hand.append(self.draw_by_number(random_number))
 
         return hand
+
+    def __len__(self):
+        '''return the number of cards in the deck '''
+        return len(self.__deck)
