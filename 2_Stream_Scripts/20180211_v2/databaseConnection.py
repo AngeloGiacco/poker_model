c8a839ccf97e564b3bc4ca65432c3c0b8567884a
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-10-25 14:49:39 +0200

    Update databaseConnection.py

--- 2_Stream_Scripts/20180211_v2/databaseConnection.py
+++ 2_Stream_Scripts/20180211_v2/databaseConnection.py
@@ -5,7 +5,7 @@ class Database:
     def __init__(self,db):
         self.conn = sqlite3.connect(db)
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS DPSpace (id integer, hand text, position integer, pot integer,chip_player1 integer,chip_player2 integer,chip_player3 integer,chip_player4 integer,chip_player5 integer,chip_player6 integer, nrGames integer, previous_action text, action text)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS DPSpace (id integer, hand text, position integer, pot integer,chip_player1 integer,chip_player2 integer,chip_player3 integer,chip_player4 integer,chip_player5 integer,chip_player6 integer, nrGames integer, previous_action text, action text, action_result integer, action_result_sum integer, action_result_avg integer)")
         self.conn.commit()
 
     def checkDP(self, hand, position, pot, chip_player1, chip_player2, chip_player3, chip_player4, chip_player5, chip_player6, nrGames, previous_action="Null"):
@@ -13,8 +13,10 @@ class Database:
         #search database for match
         self.cur.execute("SELECT * FROM book WHERE hand = ? AND position = ? AND pot = ? AND chip_player1 = ? AND chip_player2 = ? AND chip_player3 = ? AND chip_player4 = ? AND chip_player4 = ? AND chip_player5 = ? AND chip_player6 = ?", (hand,position,pot,chip_player1,chip_player2,chip_player3,chip_player4,chip_player5,chip_player6))
         rows= self.cur.fetchall()
+
         #see how many results there are
-        if len(rows) == 1: # if there does exist a decision point then return the action taken with that decision point
+        # if there does exist a decision point then return the action taken with that decision point
+        if len(rows) == 1:
             return rows[11]#index of the action column
         elif len(rows) == 0: #if there are no decision points return 0 to tell the script to add the info to the DPS.
             return 0
