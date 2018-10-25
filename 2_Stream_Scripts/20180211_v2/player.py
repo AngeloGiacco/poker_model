74958a93a699edbe118f7ff2808a55be30ec86b4
Angelo Giacco <giacco.angelo.s@gmail.com>
2018-10-25 14:51:21 +0200

    Update player.py

--- 2_Stream_Scripts/20180211_v2/player.py
+++ 2_Stream_Scripts/20180211_v2/player.py
@@ -1,20 +1,18 @@
 ''' player class '''
-
 # pylint: disable=E1601
-
+import game_init
 class Player(object):
     ''' player class '''
 
     def __init__(self, nr, chips):
         ''' player initialization '''
-        
-        self.__number = nr
+
+        self.__position_nr = nr
         self.__general_name = 'player' + str(nr)
         self.__chips = chips
         self.__hand = []
-        self.__position_nr = 0
-        self.__position_name = ''
-    
+        self.add_position(nr)
+
     def number(self):
         ''' show number '''
 
@@ -32,7 +30,7 @@ class Player(object):
 
     def add_hand(self, hand):
         ''' adding hand to player '''
-        
+
         self.__hand = hand
 
     def chips(self):
@@ -42,7 +40,7 @@ class Player(object):
 
     def increase_chips(self, win_chips):
         ''' winning money '''
-        
+
         self.__chips = self.__chips + win_chips
 
     def decrease_chips(self, lost_chips):
@@ -65,25 +63,29 @@ class Player(object):
 
         if position_nr >= 0 and position_nr <= 5:
             self.__position_nr = position_nr
-            
+
             if self.__position_nr == 0:
                 self.__position_name = 'Small Blind'
-            
+
             elif self.__position_nr == 1:
                 self.__position_name = 'Big Blind'
-            
+
             elif self.__position_nr == 2:
                 self.__position_name = 'Under the Gun'
-            
+
             elif self.__position_nr == 3:
                 self.__position_name = 'Middle'
-            
+
             elif self.__position_nr == 4:
                 self.__position_name = 'Tail'
-            
+
             else:
                 self.__position_name = 'Dealer'
-        
+
         else:
             print('Position nr is too big or too little')
             pass
+
+    def bet(self, amount):
+        game_init.game[2].increase_pot(amount)
+        self.decrease_chips(0)
